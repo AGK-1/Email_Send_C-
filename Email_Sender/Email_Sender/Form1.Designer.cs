91424d1cb2e83793267335a41a4bcf41c07cade5
Minecraft0088 <wiktorasonovic@outlook.com>
2024-08-02 00:04:57 +0400

    Send_email_with_C#

---
+++ Email_Sender/Email_Sender/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Email_Sender
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Cursor = Cursors.Hand;
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(236, 312);
+            button1.Name = "button1";
+            button1.Size = new Size(180, 60);
+            button1.TabIndex = 0;
+            button1.Text = "Send email";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(148, 22);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Email ";
+            textBox1.Size = new Size(357, 39);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(148, 115);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Your Message";
+            textBox2.ScrollBars = ScrollBars.Horizontal;
+            textBox2.Size = new Size(357, 139);
+            textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(654, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Opacity = 0.98D;
+            ShowIcon = false;
+            Text = "Send Email";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
---
+++ Email_Sender/Email_Sender/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace Email_Sender
+{
+    
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fromAddress = "your email";
+            string toAddress =textBox1.Text;// which one you want to send email to
+            string subject = "Your subject"; // subject email
+            string body = textBox2.Text; // text message
+
+
+            //If you use an email with two-step verification, this won't work
+            string smtpServer = "smtp-mail.outlook.com";
+            int smtpPort = 587; // Port for TLS/STARTTLS
+            string smtpUsername = "your email";
+            string smtpPassword = "your password"; // Use an app-specific password if 2FA is enabled
+
+            MailMessage mail = new MailMessage(fromAddress, toAddress, subject, body);
+            SmtpClient smtpClient = new SmtpClient(smtpServer, smtpPort)
+            {
+                Credentials = new NetworkCredential(smtpUsername, smtpPassword),
+                EnableSsl = true // email requires SSL
+            };
+
+            try
+            {
+                // Send the email
+                smtpClient.Send(mail);
+                MessageBox.Show("Yes Success", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (SmtpException smtpEx)
+            {
+                // Handle SMTP-specific exceptions
+                //MessageBox.Show($"SMTP Error: {smtpEx.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox2.Text= smtpEx.Message + " smtpEx Error";
+            }
+            catch (Exception ex)
+            {
+                // Handle general exceptions
+                textBox2.Text = ex.Message + " Ex Error";
+                // MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
